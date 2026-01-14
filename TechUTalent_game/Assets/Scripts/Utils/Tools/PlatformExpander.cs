9c6e4a86886b37fde5f7ccf3da3365d00009d4ae
Tenseven-107 <nadrichem@tanous.nl>
2026-01-14 17:36:08 +0100

    Level design and some bugfixes for player

---
+++ TechUTalent_game/Assets/Scripts/Utils/Tools/PlatformExpander.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+// DONT USE !!!
+
+[ExecuteAlways]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlatformExpander : MonoBehaviour
+{
+    [SerializeField] private float width = 1;
+
+    [Header("Sprite")]
+    [SerializeField] private bool useSpriteHeight;
+
+    [Header("Settings")]
+    [SerializeField] private float colliderHeight = 0.1f;
+    [SerializeField] private float colliderOffset= 0.45f;
+
+    private BoxCollider2D _coll;
+    private SpriteRenderer _platformSprite;
+
+    private void Start()
+    {
+        _coll = GetComponent<BoxCollider2D>();
+        _platformSprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnValidate()
+    {
+        var spriteSize = useSpriteHeight ? new Vector2(1, width) : new Vector2(width, 1);
+        _platformSprite.drawMode = SpriteDrawMode.Tiled;
+        _platformSprite.size = spriteSize;
+
+        _coll.size = new Vector2(width, colliderHeight);
+        _coll.offset = new Vector2(0, colliderOffset);
+    }
+}
